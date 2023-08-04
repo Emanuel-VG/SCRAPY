d420672e0c5446e194d9448643c91a0c10e61a37
Emanuel Villalobos <emanuelvillzman@gmail.com>
2023-08-04 19:11:52 -0300

    bug for :

--- course_youtube/course_youtube/spiders/chocolatespider.py
+++ course_youtube/course_youtube/spiders/chocolatespider.py
@@ -1,21 +1,22 @@
-import scrapy 
+import scrapy
 
-class ChocolatespiderSpider(scrapy.Spider)
 
-    #the name of the spider
+class ChocolatespiderSpider(scrapy.Spider):
+
+    # the name of the spider
     name = 'chocolatespider'
 
-    #the url of the first page that we will start scraping
+    # the url of the first page that we will start scraping
     start_urls = ['https://www.chocolate.co.uk/collections/all']
 
     def parse(self, response):
 
-        #here we are looping through the products and extracting the name, price & url
+        # here we are looping through the products and extracting the name, price & url
         products = response.css('product-item')
         for product in products:
-            #here we put the data returned into the format we want to output for our csv or json file
-            yield{
-                'name' : product.css('a.product-item-meta__title::text').get(),
-                'price' : product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>','').replace('</span>',''),
-                'url' : product.css('div.product-item-meta a').attrib['href'],
+            # here we put the data returned into the format we want to output for our csv or json file
+            yield {
+                'name': product.css('a.product-item-meta__title::text').get(),
+                'price': product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>', '').replace('</span>', ''),
+                'url': product.css('div.product-item-meta a').attrib['href'],
             }
